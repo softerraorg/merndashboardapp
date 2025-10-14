9fe723b9537abb7c33e4b73232fdd40a235d1421
profituned <profituned@gmail.com>
2025-10-15 02:59:54 +0500

    Fix Users component DataTable implementation

--- web/frontend/pages/Users.jsx
+++ web/frontend/pages/Users.jsx
@@ -1,14 +1,52 @@
 import {
     Page,
+    Layout,
+    LegacyCard,
+    DataTable,
   } from "@shopify/polaris";
+  import { useEffect, useState } from "react";
+
   
   
   
   export default function Users() {
-  
-    return (
-      <Page narrowWidth>
-        Userspage
-      </Page>
-    );
+
+    let [users, setUsers] = useState([]);
+    let getUsers = () => {
+
+   fetch('api/getusers')
+      .then(response => response.json())
+      .then(data => {setUsers(data);console.log("users data", data)})
+      .catch(error => console.error('Error:', error));
+    }
+
+    useEffect(() => {
+      getUsers();
+    }, []);
+
+  return (
+
+    <Page narrowWidth>
+      <Layout>
+        <Layout.Section>
+          <LegacyCard title="Users" sectioned>
+            <DataTable 
+              columnContentTypes={[
+                'text',
+                'text',
+              ]}
+              headings={[
+                'Username',
+                'Email',
+              ]}
+              rows={users.map((user) => [
+                user.username,
+                user.useremail,
+              ])}
+            />
+          </LegacyCard>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
   }
